8684f18c3ded22fdce44dfa841ef720a2a0ac4a9
eandr-67 <eandr-67@mail.ru>
2026-07-29 10:42:12 +0300

    Stringer implementation

--- stringer.go
+++ stringer.go
@@ -8,8 +8,13 @@ import (
 func (s Set[T]) String() string {
 	t := strings.Builder{}
 	t.WriteRune('{')
+	flg := false
 	for k := range s {
+		if flg {
+			t.WriteString(", ")
+		}
 		t.WriteString(fmt.Sprint(k))
+		flg = true
 	}
 	t.WriteRune('}')
 	return t.String()
